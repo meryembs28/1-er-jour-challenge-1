a76eeb470254875a932061b61c7168f19081ee16
Meryem Bouras <bourasmeryem03@gmail.com>
2025-09-02 16:40:48 +0100

    ex 12

---
+++ 12.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int N , n1 ,n2 , n3 , n4 ;
+    printf("veuillez saisir un nbr de 4 chiffres svp :");
+    scanf("%d", &N);
+    n1= N % 10;
+    n2 = (N / 10) % 10;
+    n3 = (N / 100) % 10;
+    n4 = (N / 1000);
+
+    printf("l inverse de ce nbr = %d%d%d%d", n1,n2,n3,n4);
+    return 0 ;
+}
---
+++ 13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int D , B , H ;
+    printf("veuillez saisir le nbr en decimal svp :");
+    scanf("%d", &D);
+    do {
+
+    }
+    while(D%2>0)
+  
+    return 0 ;
+}
