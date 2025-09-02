b3e1a31d49a6bdd3d4ae2a0c97e1d1fc2cefaa46
Meryem Bouras <bourasmeryem03@gmail.com>
2025-09-02 14:24:50 +0100

    ex 2

--- 2.c
+++ 2.c
@@ -1,1 +1,11 @@
-test
+#include <stdio.h>
+
+int main() {
+    float k , c;
+    const float d=273.15;
+    printf("veuillez saisir la temperature  en Celsius  \n");
+    scanf("%f", &c);
+    k = c + d;
+    printf ("la temperature en Kelvin est : %f ", k);
+    return 0;
+}
---
+++ 3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float k , y;
+    const float d=1093.61;
+    printf("veuillez saisir la distance  en Kilometres   \n");
+    scanf("%f", &k);
+    y =  k * d;
+    printf (" la distance  en Yards est : %.2f ", y);
+    return 0;
+}
